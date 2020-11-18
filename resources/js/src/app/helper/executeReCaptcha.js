c4577de26fdcf3194a93aae9453ab38d4eb4c1eb
Lukas Matzen <lukas.matzen@plentymarkets.com>
2020-11-18 16:30:58 +0100

    FIX reject recaptcha when cookie is not accepted

--- resources/js/src/app/helper/executeReCaptcha.js
+++ resources/js/src/app/helper/executeReCaptcha.js
@@ -1,49 +1,51 @@
 export function executeReCaptcha(form)
 {
-    let response = Promise.resolve(null);
     const recaptchaElement = form.querySelector("[data-recaptcha]");
 
-    if (window.grecaptcha && recaptchaElement)
+    const response = new Promise((resolve, reject) =>
     {
-        response = new Promise((resolve, reject) =>
+        if (!window.grecaptcha || !recaptchaElement)
         {
-            if (App.config.global.googleRecaptchaVersion === 3)
+            reject();
+            return;
+        }
+
+        if (App.config.global.googleRecaptchaVersion === 3)
+        {
+            window.grecaptcha.execute(
+                App.config.global.googleRecaptchaApiKey,
+                { action: "homepage" }
+            ).then(response =>
             {
-                window.grecaptcha.execute(
-                    App.config.global.googleRecaptchaApiKey,
-                    { action: "homepage" }
-                ).then(response =>
+                if (response)
                 {
-                    if (response)
+                    resolve(response);
+                }
+                else
+                {
+                    reject();
+                }
+            });
+        }
+        else
+        {
+            window.grecaptcha.execute(recaptchaElement.dataset.recaptcha);
+            recaptchaElement
+                .querySelector("[name=\"g-recaptcha-response\"]")
+                .addEventListener("recaptcha-response", (evt) =>
+                {
+                    if (evt.target.value)
                     {
-                        resolve(response);
+                        resolve(evt.target.value);
                     }
                     else
                     {
+                        window.grecaptcha.reset(recaptchaElement.dataset.recaptcha);
                         reject();
                     }
                 });
-            }
-            else
-            {
-                window.grecaptcha.execute(recaptchaElement.dataset.recaptcha);
-                recaptchaElement
-                    .querySelector("[name=\"g-recaptcha-response\"]")
-                    .addEventListener("recaptcha-response", (evt) =>
-                    {
-                        if (evt.target.value)
-                        {
-                            resolve(evt.target.value);
-                        }
-                        else
-                        {
-                            window.grecaptcha.reset(recaptchaElement.dataset.recaptcha);
-                            reject();
-                        }
-                    });
-            }
-        });
-    }
+        }
+    });
 
     return response;
 }
