ddb6bbf95064953c4319073a40c49481816c366a
Lukas Matzen <lukas.matzen@plentymarkets.com>
2021-01-19 14:45:41 +0100

    FIX address input vat id validation

--- resources/js/src/app/components/customer/VatId.vue
+++ resources/js/src/app/components/customer/VatId.vue
@@ -6,7 +6,7 @@
         <div class="input-unit border-0 w-auto input-group-prepend">
             <span class="input-group-text h-100" id="basic-addon1">{{ vatPrefix }}</span>
         </div>
-        <div class="input-unit flex-fill w-auto">
+        <div class="input-unit flex-fill w-auto" v-validate:text="isRequired">
             <input aria-describedby="basic-addon1" type="text" name="vatNumber" :id="'txtVatNumber' + _uid" v-model="vatValue" data-autofocus>
             <label :for="'txtVatNumber' + _uid">
                 {{ transformTranslation("Ceres::Template.addressVatNumber", "de", "billing_address.vatNumber") }}
